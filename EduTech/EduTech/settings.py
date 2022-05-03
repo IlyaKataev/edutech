8322cab5b7ad40cf4d7a84c1a2dae7d7975deedf
ilya <k4t4eff@gmail.com>
2022-05-03 13:40:08 +0300

    delete caching

--- EduTech/EduTech/settings.py
+++ EduTech/EduTech/settings.py
@@ -86,6 +86,11 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
