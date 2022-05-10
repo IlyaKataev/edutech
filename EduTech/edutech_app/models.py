791d47185a994881afac982adf1b4ab73f892ec8
ilya <k4t4eff@gmail.com>
2022-05-10 11:46:41 +0300

    models refactoring

--- EduTech/edutech_app/models.py
+++ EduTech/edutech_app/models.py
@@ -7,24 +7,24 @@ class User(AbstractUser):
 
 
 class Subject(models.Model):
-    Name = models.TextField(max_length=50)
+    name = models.TextField(max_length=50)
 
 
 class Teacher(models.Model):
-    Name = models.TextField(max_length=50)
-    Avatar = models.ImageField
-    Description = models.TextField(max_length=800)
+    name = models.TextField(max_length=50)
+    avatar = models.ImageField
+    description = models.TextField(max_length=800)
     subject = models.ManyToManyField(Subject)
     objects = models.Manager()
 
 
 class ClassNumber(models.Model):
-    Number = models.TextField(max_length=50)
+    number = models.TextField(max_length=50)
 
 
 class Course(models.Model):
-    Name = models.TextField(max_length=50)
-    Description = models.TextField(max_length=800)
+    name = models.TextField(max_length=50)
+    description = models.TextField(max_length=800)
     subject = models.ForeignKey(to=Subject, on_delete=models.CASCADE)
     classNumber = models.ForeignKey(to=ClassNumber, on_delete=models.CASCADE)
     teacher = models.ForeignKey(to=Teacher, on_delete=models.CASCADE)
