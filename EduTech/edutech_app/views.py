dd48f194baae884e11dd177571bbc269a74d0bc7
ilya <k4t4eff@gmail.com>
2022-06-03 16:39:10 +0300

    finally improve menu

--- EduTech/edutech_app/views.py
+++ EduTech/edutech_app/views.py
@@ -7,10 +7,21 @@ from django.http import HttpResponseRedirect
 from .models import Course, CurrentCourse
 
 
-def get_base_context():
-    return {
+def get_base_context(user_is_authed):
+    context = {
         'courses': Course.objects.all(),
     }
+    if not user_is_authed:
+        buttons = {
+            '/accounts/register': 'sign up',
+            '/accounts/login': 'sign in',
+        }
+    else:
+        buttons = {
+            '/accounts/logout': 'sign out',
+        }
+    context['buttons'] = buttons
+    return context
 
 
 class Register(View):
@@ -19,12 +30,12 @@ class Register(View):
     def get(self, request):
         if request.user.is_authenticated:
             return HttpResponseRedirect('/')
-        context = get_base_context()
+        context = get_base_context(request.user.is_authenticated)
         context['form'] = UserCreationForm()
         return render(request, self.template_name, context)
 
     def post(self, request):
-        context = get_base_context()
+        context = get_base_context(request.user.is_authenticated)
         form = UserCreationForm(request.POST)
         context['form'] = form
 
@@ -41,12 +52,12 @@ class Register(View):
 
 @login_required
 def show_stream(request):
-    context = get_base_context()
+    context = get_base_context(request.user.is_authenticated)
     return render(request, 'stream.html', context)
 
 
 def show_main(request):
-    context = get_base_context()
+    context = get_base_context(request.user.is_authenticated)
     if request.user.is_authenticated:
         context['url'] = '/courses'
     else:
@@ -56,7 +67,7 @@ def show_main(request):
 
 @login_required
 def show_courses(request):
-    context = get_base_context()
+    context = get_base_context(request.user.is_authenticated)
     return render(request, 'courses.html', context)
 
 
@@ -68,14 +79,14 @@ def show_course(request, course_id):
                                        class_number=Course.objects.get(id=course_id).class_number.number,
                                        email=request.user.email)
         current_course.save()
-    context = get_base_context()
+    context = get_base_context(request.user.is_authenticated)
     context['course'] = Course.objects.get(id=course_id)
     return render(request, 'course.html', context)
 
 
 @login_required
 def show_profile(request):
-    context = get_base_context()
+    context = get_base_context(request.user.is_authenticated)
     context['name'] = request.user.username
     context['email'] = request.user.email
     context['current_courses'] = CurrentCourse.objects.filter(email=request.user.email)
